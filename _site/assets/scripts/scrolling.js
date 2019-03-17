7b125fc6feca2f91ef5a0b7dc19e8517035cc1d5
marigiel <marie_gielen@hotmail.com>
2019-03-17 15:22:14 +0100

    css

--- _site/assets/scripts/scrolling.js
+++ _site/assets/scripts/scrolling.js
@@ -6,3 +6,13 @@ $('a[href^="#"]').click(function () {
     return false;
 });
 
+$('img[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 300);
+
+    return false;
+});
+
+
+
