938d9f895795448949209d7c421f1e57c9849e04
marigiel <marie_gielen@hotmail.com>
2019-03-17 13:32:09 +0100

    css test 4 (plus goeie scroll)

---
+++ _site/assets/scripts/scrolling.js
@@ -0,0 +1,8 @@
+$('a[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 300);
+
+    return false;
+});
+
