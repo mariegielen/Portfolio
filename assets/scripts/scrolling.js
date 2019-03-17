69182a9126a3060f3d7faa70636506e03a8cb394
marigiel <marie_gielen@hotmail.com>
2019-03-17 16:36:26 +0100

    why

--- assets/scripts/scrolling.js
+++ assets/scripts/scrolling.js
@@ -6,13 +6,6 @@ $('a[href^="#"]').click(function () {
     return false;
 });
 
-$('img[href^="#"]').click(function () {
-    $('html, body').animate({
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 300);
-
-    return false;
-});
 
 
 
